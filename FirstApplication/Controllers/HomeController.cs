05e181551074a9dcf4fdf6c6b285d2a0124a2b71
Pham Luong Son Phi <phamluongsonphi@gmail.com>
2022-10-31 13:51:42 +0700

    Add Test Page

--- FirstApplication/Controllers/HomeController.cs
+++ FirstApplication/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace FirstApplication.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
